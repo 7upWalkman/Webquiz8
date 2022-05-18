4024f874e1da1d2f1b142c1bab8bc2df01626bc5
7upWalkman <91927498+7upWalkman@users.noreply.github.com>
2022-05-18 14:02:30 +0100

    Add files via upload

---
+++ docs/exemplos/context_managers.py
@@ -0,0 +1,43 @@
+from contextlib import contextmanager
+
+@contextmanager
+def cmA():
+    print("Acquiring A")
+    try:
+        yield 'A'
+    finally:
+        print("Releasing A")
+
+@contextmanager
+def cmB():
+    print("Acquiring B")
+    try:
+        yield 'B'
+    finally:
+        print("Releasing B")
+
+
+def test_a_b():
+    with cmA():
+        with cmB():
+            print("TEST_A_B")
+
+
+@contextmanager
+def cmX():
+    with cmA():
+        with cmB():
+            print("Aquiring X")
+            try: 
+                yield 'X'
+            finally:
+                print("Releasing X")
+
+@contextmanager
+def cmXX():
+    with cmA():
+        with cmB():
+            print("Aquiring X")
+            yield 'X'
+            print("Releasing X")
+
